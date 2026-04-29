35404c499b99e36197249cc5939791841683840b
Shashikr74 <150127990+Shashikr74@users.noreply.github.com>
2026-04-29 20:51:10 +0530

    Create calculator_backend.py

---
+++ calculator_backend.py
@@ -0,0 +1,27 @@
+# file: calculator_backend.py
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero"
+    return a / b
+
+def calculate(a, b, op):
+    if op == '+':
+        return add(a, b)
+    elif op == '-':
+        return subtract(a, b)
+    elif op == '*':
+        return multiply(a, b)
+    elif op == '/':
+        return divide(a, b)
+    else:
+        return "Invalid Operation"
